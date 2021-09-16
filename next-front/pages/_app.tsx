a5887c5c41356a07498fa8eab4487133c0045ffb
“Rockytcp” <gustavo.braganti@gmail.com>
2021-09-16 16:42:28 -0300

    Integracao com microsservico nestjs e keycloak para consumir API get visando obter todas as transactions registradas com o usuário logado

--- next-front/pages/_app.tsx
+++ next-front/pages/_app.tsx
@@ -1,21 +1,42 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppContext, AppProps } from "next/app";
 import theme from "../utils/theme";
 import { ThemeProvider } from "@material-ui/styles";
 import { CssBaseline } from "@material-ui/core";
 import { useEffect } from "react";
+import { SSRKeycloakProvider, SSRCookies } from "@react-keycloak/ssr";
+import { KEYCLOAK_PUBLIC_CONFIG } from "../utils/auth";
+import { parseCookies } from "../utils/cookies";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, cookies }: AppProps & { cookies: any }) {
   useEffect(() => {
     const jssStyles = document.querySelector("#jss-server-side");
     jssStyles?.parentElement?.removeChild(jssStyles);
   }, []);
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />{" "}
-    </ThemeProvider>
+    <SSRKeycloakProvider
+      keycloakConfig={KEYCLOAK_PUBLIC_CONFIG}
+      persistor={SSRCookies(cookies)}
+      initOptions={{
+        onLoad: "check-sso",
+        silentCheckSsoRedirectUri:
+          typeof window !== "undefined"
+            ? `${window.location.origin}/silent-check-sso.html`
+            : null,
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />{" "}
+      </ThemeProvider>
+    </SSRKeycloakProvider>
   );
 }
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  return {
+    cookies: parseCookies(appContext.ctx.req),
+  };
+};
 export default MyApp;
